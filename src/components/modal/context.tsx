6881bad0cef40e7d616211f096bf324694b84633
Olifem <olifemtsx@gmail.com>
2023-10-30 12:11:32 +0300

    asd

--- src/components/modal/context.tsx
+++ src/components/modal/context.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import Modal from "./modal";
+import Modal, {IModal} from "./modal";
 import {useModal} from "@/components/modal/useModal";
+import {ReactNode} from "@/common/types";
 
 let ModalContext;
 const {Provider} = (ModalContext = React.createContext({}));
 
+interface IModalContext {
+    children: ReactNode
+}
 
-const ModalProvider = ({children}) => {
+const ModalProvider:React.FC<IModal> = ({children}) => {
     const {modal, handleModal, modalContent} = useModal();
 
     return (
