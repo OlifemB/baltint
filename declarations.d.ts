de9a74840ff9726885be60602255083b9f782e3d
Olifem <olifemtsx@gmail.com>
2023-10-30 13:48:00 +0300

    asd

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,5 +1,5 @@
 declare module "redux-persist/es/storage" {
-    import { WebStorage } from "redux-persist/es/types";
+    import {WebStorage} from "redux-persist/es/types";
 
     const localStorage: WebStorage;
     export default localStorage;
@@ -7,5 +7,7 @@ declare module "redux-persist/es/storage" {
 
 declare module "redux-persist/lib/storage" {
     export * from "redux-persist/es/storage";
-    export { default } from "redux-persist/es/storage";
+    export {default} from "redux-persist/es/storage";
 }
+
+declare module "redux-persist/lib/storage/createWebStorage";
