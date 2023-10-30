c95fe8e8abc6fd31b3e6bc4004f20116674abe17
Olifem <olifemtsx@gmail.com>
2023-10-30 13:30:00 +0300

    asd

--- src/common/types.ts
+++ src/common/types.ts
@@ -5,4 +5,4 @@ type ReactChild = ReactElement | ReactText;
 
 interface ReactNodeArray extends Array<ReactNode> {}
 type ReactFragment = {} | ReactNodeArray;
-export type ReactNode = ReactChild | ReactFragment | ReactPortal | boolean | null | undefined;
+export type ReactNode = ReactChild | ReactFragment | ReactPortal | boolean | null | undefined | {};
