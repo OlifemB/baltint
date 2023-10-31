ceef264adac632bf478f6f51838292a42761b6d3
Olifem <olifemtsx@gmail.com>
2023-10-31 12:34:46 +0300

    asd

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2cf83dedd84ec952"; }
+/******/ 		__webpack_require__.h = function() { return "e60495917e7e53f8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -43,7 +43,7 @@ const Header = () => {
                             <Link
                                 replace={true}
                                 href={item.link}
-                                className={clsx('font-normal hover:text-gray-100  duration-300', item.id === pathname.slice(1) ||pathname === '/' && item.id === 'main' ? 'text-white' : 'text-gray-300 ')}
+                                className={clsx('font-normal hover:text-gray-100  duration-300', item.id === pathname.slice(1) ||pathname === '/' && item.id === 'main' ? 'text-white [text-shadow:_0_0_10px_rgb(255_255_255_/_40%)]' : 'text-gray-300 ')}
                             >
                                 {item.title}
                             </Link>
